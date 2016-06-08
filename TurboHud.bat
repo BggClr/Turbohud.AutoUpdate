f74fb79168e4fb565cd78922e507c1abeb929282
Danil.Zhelnov <danil.zhelnov@engine6.com>
2016-06-09 00:58:56 +0500

    added batch file to the release

---
+++ TurboHud.bat
@@ -0,0 +1,2 @@
+call Turbohud.AutoUpdate.exe
+call Turbohud.exe
--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 packages\ILRepack.2.0.10\tools\ILRepack.exe /ndebug /allowdup:AngleSharp.ConfigurationExtensions /wildcards /targetplatform:v4 /out:"release\_tmp\Turbohud.AutoUpdate.exe" Turbohud.AutoUpdate\bin\Turbohud.AutoUpdate.exe Turbohud.AutoUpdate\bin\*.dll
-
+copy TurboHud.bat release\_tmp\TurboHud.bat
 pushd release
 	pushd _tmp
 		..\..\packages\7-Zip.CommandLine.9.20.0\tools\7za.exe -tzip a release.zip
